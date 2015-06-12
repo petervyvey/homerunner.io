29e4b1f5c00a9077adfb1ac140e21eb53c96940a
Peter Vyvey <peter.vyvey@gmail.com>
2015-06-12 22:36:28 +0200

    HomeRunner.Web.Host project added.

---
+++ src/HomeRunner.Web.Host/Controllers/PlatformController.cs
@@ -0,0 +1,14 @@
+﻿
+using System.Web.Mvc;
+
+namespace HomeRunner.Web.Host.Controllers
+{
+    public class PlatformController : Controller
+    {
+        // GET: Platform
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/HomeRunner.Web.Host/Global.asax.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace HomeRunner.Web.Host
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            //WebApiConfig.Register(GlobalConfiguration.Configuration);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+
+    }
+}
