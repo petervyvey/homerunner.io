51e2973d9931b06d8c1c5f994236e144265c6d7c
Peter Vyvey <peter.vyvey@gmail.com>
2015-06-15 06:51:47 +0200

    * HomeRunner.Web.Host.csproj: Added dependency OWIN web host   package.
    * Startup.cs:
    * packages.config:

--- src/HomeRunner.Web.Host/Startup.cs
+++ src/HomeRunner.Web.Host/Startup.cs
@@ -2,8 +2,9 @@
 using HomeRunner.Web.Host;
 using Microsoft.Owin;
 using Owin;
+using System.Diagnostics;
 
-[assembly: OwinStartup(typeof(Startup))]
+//[assembly: OwinStartup(typeof(Startup))]
 
 namespace HomeRunner.Web.Host
 {
@@ -12,6 +13,7 @@ namespace HomeRunner.Web.Host
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+			Debug.WriteLine(this);
         }
     }
 }
