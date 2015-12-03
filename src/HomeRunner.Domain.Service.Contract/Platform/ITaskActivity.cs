5e65624893857a3a68a193206dc966e1b3250bf7
Peter Vyvey <peter.vyvey@gmail.com>
2015-12-03 21:49:09 +0100

    Added comments.

--- src/HomeRunner.Domain.Service.Contract/Platform/ITaskActivity.cs
+++ src/HomeRunner.Domain.Service.Contract/Platform/ITaskActivity.cs
@@ -3,6 +3,9 @@ using System;
 
 namespace HomeRunner.Domain.Service.Platform
 {
+	/// <summary>
+	/// Interface defining the exposed properties of a task domain object.
+	/// </summary>
     public interface ITaskActivity
     {
         /// <summary>
@@ -10,10 +13,22 @@ namespace HomeRunner.Domain.Service.Platform
         /// </summary>
         Guid Id { get; set; }
 
+		/// <summary>
+		/// The task description.
+		/// </summary>
+		/// <value>The description.</value>
         string Description { get; }
 
+		/// <summary>
+		/// The person the task is assigned to.
+		/// </summary>
+		/// <value>The assigned to.</value>
         string AssignedTo { get; }
 
+		/// <summary>
+		/// TRUE if the task is claimed, FALSE otherwise.
+		/// </summary>
+		/// <value><c>true</c> if this instance is claimed; otherwise, <c>false</c>.</value>
         bool IsClaimed { get; }
 
         bool IsCompleted { get; }
