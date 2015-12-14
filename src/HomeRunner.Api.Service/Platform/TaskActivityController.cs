137f0d318310a9542e69ed7ed6a82caca79f3513
Peter Vyvey <peter.vyvey@gmail.com>
2015-12-14 20:15:03 +0100

    Seperated RabbitMQ configuration

--- src/HomeRunner.Api.Service/Platform/TaskActivityController.cs
+++ src/HomeRunner.Api.Service/Platform/TaskActivityController.cs
@@ -72,7 +72,7 @@ namespace HomeRunner.Api.Service.Platform
 			CreateTaskActivityCommand command = new CreateTaskActivityCommand(intent.Description);
 
 			HomeRunner.Api.Service.Logger.Log.Info(string.Format("Submiting command: {0} [{1}] ", command.GetType().FullName, command.Id));
-			ISendEndpoint endpoint = await this.bus.GetSendEndpoint(RabbitMQConfiguration.Exchange);
+			ISendEndpoint endpoint = await this.bus.GetSendEndpoint(Foundation.RabbitMQ.Configuration.Exchange);
 			await endpoint.Send(new CommandMessage<CreateTaskActivityCommand>(command));
 
 			HomeRunner.Api.Service.Logger.Log.Info(string.Format("Command submitted: {0} [{1}]", command.GetType().FullName, command.Id));
@@ -85,7 +85,7 @@ namespace HomeRunner.Api.Service.Platform
         {
             ClaimTaskActivityCommand command = new ClaimTaskActivityCommand(id);
 
-			ISendEndpoint endpoint = await this.bus.GetSendEndpoint(RabbitMQConfiguration.Exchange);
+			ISendEndpoint endpoint = await this.bus.GetSendEndpoint(Foundation.RabbitMQ.Configuration.Exchange);
             await endpoint.Send(new CommandMessage<ClaimTaskActivityCommand>(command));
 
             return ApiResponse.CommandResponse(command);
@@ -96,7 +96,7 @@ namespace HomeRunner.Api.Service.Platform
         {
             UnclaimTaskActivityCommand command = new UnclaimTaskActivityCommand(id);
 
-			ISendEndpoint endpoint = await this.bus.GetSendEndpoint(RabbitMQConfiguration.Exchange);
+			ISendEndpoint endpoint = await this.bus.GetSendEndpoint(Foundation.RabbitMQ.Configuration.Exchange);
             await endpoint.Send(new CommandMessage<UnclaimTaskActivityCommand>(command));
 
             return ApiResponse.CommandResponse(command);
