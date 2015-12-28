3c0408f4bb2a22a426afe32be0fe4034b64f603a
Peter Vyvey <peter.vyvey@gmail.com>
2015-12-28 22:40:20 +0100

    C# 6.0 test

---
+++ src/HomeRunner.Foundation.NextGen.Host/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using static System.Console;
+
+namespace HomeRunner.Foundation.NextGen.Host
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Test");
+
+            var o = new Organisation();
+            var p = default(Person);
+            o.Add(Guid.NewGuid(), p);
+        }
+    }
+}
---
+++ src/HomeRunner.Foundation.NextGen/Person.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace HomeRunner.Foundation.NextGen
+{
+    public class Person
+    {
+        public Person(Guid id)
+        {
+            this.Id = id;
+        }
+
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string FirstName { get; set; }
+
+        public int AgeInDays(int years) => years * 365;
+
+        public string FullName { get { return $"{Name} {FullName}"; } }
+    }
+
+    public class Organisation
+    {
+        public Dictionary<Guid, Person> Persons { get; private set; } = new Dictionary<Guid, Person>();
+
+        public Person Add(Guid id, Person person)
+        {
+            this.Persons.Add(person?.Id ?? id, person ?? new Person(id));
+
+            return this.Persons[id];
+        }
+
+    }
+}
