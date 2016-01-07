67b6a350e18d3cafa92234e3afb76e4958f61af5
Peter Vyvey <peter.vyvey@gmail.com>
2016-01-07 06:59:49 +0100

    Moved common cache code to single class.

---
+++ src/HomeRunner.Foundation/Decorator/DomainEventPublisherTypeCache.cs
@@ -0,0 +1,11 @@
+﻿
+using System;
+using System.Collections.Concurrent;
+
+namespace HomeRunner.Foundation.Decorator
+{
+    public class DomainEventPublisherTypeCache
+    {
+        internal static readonly ConcurrentDictionary<Type, Type> MESSAGE_TYPES = new ConcurrentDictionary<Type, Type>();
+    }
+}
--- src/HomeRunner.Foundation/Decorator/InternalDomainEventPublisherDecorator.cs
+++ src/HomeRunner.Foundation/Decorator/InternalDomainEventPublisherDecorator.cs
@@ -5,7 +5,6 @@ using HomeRunner.Foundation.Infrastructure.Extension;
 using HomeRunner.Foundation.Infrastructure.Logging;
 using MediatR;
 using System;
-using System.Collections.Concurrent;
 using System.Linq;
 
 namespace HomeRunner.Foundation.Decorator
@@ -15,7 +14,7 @@ namespace HomeRunner.Foundation.Decorator
         where TCommand : ICommand<TCommandResult>, IRequest<TCommandResult>
         where TCommandResult : class, ICommandResult
     {
-        protected static readonly ConcurrentDictionary<Type, Type> MESSAGE_TYPES = new ConcurrentDictionary<Type, Type>();
+
 
         private readonly IMediator mediator;
 
@@ -35,25 +34,25 @@ namespace HomeRunner.Foundation.Decorator
             Logger.Log.InfoFormat(Logger.CORRELATED_LONG_CONTENT, command.Id, "received command", _command);
             TCommandResult events = this.handler.Handle(command);
 
-            Logger.Log.InfoFormat(Logger.CORRELATED_LONG_CONTENT, command.Id, "internal publish domain events", _command);
+            Logger.Log.InfoFormat(Logger.CORRELATED_LONG_CONTENT, command.Id, "publish domain events internal", _command);
             events.ToList().ForEach(de =>
             {
                 var _domainEvent = de.ToJson();
 
-                Logger.Log.InfoFormat(Logger.CORRELATED_LONG_CONTENT, command.Id, "publish domain event", _domainEvent);
+                Logger.Log.InfoFormat(Logger.CORRELATED_LONG_CONTENT, command.Id, "domain event", _domainEvent);
 
-                Logger.Log.InfoFormat(Logger.CORRELATED_CONTENT, command.Id, "find domain event type message type", de.GetType());
+                Logger.Log.InfoFormat(Logger.CORRELATED_CONTENT, command.Id, "find domain event type message type for ", de.GetType().FullName);
                 Type messageType;
-                if (!MESSAGE_TYPES.TryGetValue(de.GetType(), out messageType))
+                if (!DomainEventPublisherTypeCache.MESSAGE_TYPES.TryGetValue(de.GetType(), out messageType))
                 {
                     Logger.Log.WarnFormat(Logger.CORRELATED_CONTENT, command.Id, "domain event message type not found", de.GetType());
 
-                    Logger.Log.WarnFormat(Logger.CORRELATED_CONTENT, command.Id, "create domain event type", de.GetType());
-                    messageType = MESSAGE_TYPES[de.GetType()] = typeof(DomainEventMessage<>).MakeGenericType(de.GetType());
+                    Logger.Log.WarnFormat(Logger.CORRELATED_CONTENT, command.Id, "create domain event message type", de.GetType());
+                    messageType = DomainEventPublisherTypeCache.MESSAGE_TYPES[de.GetType()] = typeof(DomainEventMessage<>).MakeGenericType(de.GetType());
 
                     Logger.Log.WarnFormat(Logger.CORRELATED_CONTENT, command.Id, "domain event message type created", messageType.FullName);
                 }
-                Logger.Log.DebugFormat(Logger.CORRELATED_CONTENT, command.Id, "domain event message type found", messageType.FullName);
+                Logger.Log.DebugFormat(Logger.CORRELATED_CONTENT, command.Id, "domain event message type found", messageType.Name);
 
                 INotification message = Activator.CreateInstance(messageType, de) as INotification;
 
